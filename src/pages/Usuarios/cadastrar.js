e67dd7c39a2225d9b57938feb12b40a34bcc29fd
Maiquel Leites <maiquelveve2008@gmail.com>
2020-03-03 17:41:08 -0300

    iniciado a parte das mgs de success or error

--- src/pages/Usuarios/cadastrar.js
+++ src/pages/Usuarios/cadastrar.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { autenticado } from '../../services/auth';
+import api from '../../services/api';
 
 function Cadastrar(props) {
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
     const [nome, setNome] = useState('')
+    const [erros, setErros] = useState([])
+    const [success, setSuccess] = useState([])
 
     //Para não permitir que usuarios logados acessem essa página, só lembrando que isso funciona com o method ComponentDidMount()
     useEffect(
@@ -16,16 +19,52 @@ function Cadastrar(props) {
         []
     )
 
-    function cadastrar(e){
+    async function cadastrar(e){
         e.preventDefault()
+        let usuario = {
+            st_nome: nome,
+            st_email: email,
+            st_senha: senha
+        }
+
+        try {
+            let resultado = await api.post('/usuarios/cadastrar', usuario)
+            
+            if(resultado.data[0].success >= 1) {
+                setSuccess(['Usuário Cadastrado com sucesso!'])
+            } else {
+                resultado.data.shift()
+                setErros(resultado.data)
+            }
+
+        } catch (error) {
+            alert('Hovem algum problema tente novamente mais tarde')
+        }
     }
 
     return (
         <div className="container-fluid h-100 mt-5">
+            {erros.length !== 0 &&
+                
+                <div className="alert alert-danger">
+                    <ul>
+                        <li>ERROS</li>
+                    </ul>
+                </div>
+                   
+            }
+             
+            {success.length !== 0 &&
+                <div className="alert alert-success">
+                    <ul>
+                        <li>SUCESSO</li>
+                    </ul>
+                </div>
+            }
             <div className="row justify-content-center align-items-center h-100">
                 <div className="col col-sm-6 col-md-6 col-lg-4 col-xl-3">
                     <form onSubmit={cadastrar}>
-                    <div className="form-group">
+                        <div className="form-group">
                             <label>Nome</label>
                             <input className="form-control form-control-lg" value={nome} onChange={e => setNome(e.target.value)} placeholder="Informe o Nome" type="text" />
                         </div>
