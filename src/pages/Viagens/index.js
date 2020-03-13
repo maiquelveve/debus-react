490c9fe1298fda1b3d2fc5755b15d52aaac6915d
Maiquel Leites <maiquelveve2008@gmail.com>
2020-03-13 17:57:36 -0300

    viagens com validacao de tyoken

--- src/pages/Viagens/index.js
+++ src/pages/Viagens/index.js
@@ -1,9 +1,27 @@
-import React, {useEffect, useCallback, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {withRouter} from 'react-router-dom';
 
-import {validaTokenApi, autenticado}  from '../../services/auth';
+import { validaToken }  from '../../services/auth';
 
 function Viagens() {
+
+    const [validacaoToken, setValidacaoToken] = useState(true)
+
+    useEffect(
+        () => {
+            async function fetchData() {
+                const token = await validaToken();
+
+                if(!token) {
+                    setValidacaoToken(token)
+                    window.location.reload('/')
+                }
+            }
+            fetchData();
+        },
+        []
+    )
+    
     return (
         <div>
             <h1>Viagens</h1>
