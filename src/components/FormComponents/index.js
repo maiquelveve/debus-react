fb39d70279c273d5a9739ed43f95c3a6cf5fedc5
Maiquel Leites <maiquelveve2008@gmail.com>
2020-05-13 16:34:46 -0300

    testando material UI

---
+++ src/components/FormComponents/index.js
@@ -0,0 +1,14 @@
+//Para usar o MATERIAL-UI deve ser intalado as seguintes dependências
+// yarn add @material-ui/core  
+// yarn add @material-ui/icons
+// yarn add typeface-roboto
+// Para ficar responsivo para web e mobile use isso <meta name="viewport"content="minimum-scale=1, initial-scale=1, width=device-width"/>
+
+import React from 'react';
+import {Button} from '@material-ui/core';
+
+export const ButtonEX = () => (
+    <Button variant="contained" color="primary" >
+      Disable elevation
+    </Button>
+)
--- src/pages/Viagens/reservar.js
+++ src/pages/Viagens/reservar.js
@@ -6,6 +6,11 @@ import api from '../../services/api';
 import { AlertCatch } from '../../components/AlertasDefaultSistema';
 
 import { Loading, ExibirLoadingLayout } from '../../components/Loading'
+import { ButtonEX } from '../../components/FormComponents'
+
+
+import {Grid, IconButton } from '@material-ui/core';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 function Reservar(props) {
     const [viagem, setViagem] = useState([])
@@ -38,8 +43,26 @@ function Reservar(props) {
 
     return(
         <div>
-            <ExibirLoadingLayout size={95} />
+            {/* <ExibirLoadingLayout size={95} /> */}
             <p>{viagem.id}</p>
+
+            <Grid container spacing={3} direction="row" justify="center" alignItems="center">
+                <Grid item xs={12} >
+                    <ButtonEX titulo="OLA" />  
+                </Grid>
+                <Grid item xs={6}>
+                    <ButtonEX titulo="OLA" />  
+                </Grid>
+                <Grid item xs={6} >
+                    <ButtonEX titulo="OLA" />  
+                </Grid>
+            </Grid> 
+
+            <IconButton>
+                <DeleteIcon />
+            </IconButton>
+            
+
         </div>
     )
 }
