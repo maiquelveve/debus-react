af39ab521db00a00ce34374e9cb998117436e9aa
maiquelveve <maiquelveve2008@gmail.com>
2020-05-13 22:45:48 -0300

    criada o componete com dois tipo de campos de datas

--- src/components/FormComponents/datas.js
+++ src/components/FormComponents/datas.js
@@ -7,60 +7,81 @@
             * yarn add @material-ui/pickers
 */
 
+import React, {useState} from 'react';
 import 'date-fns';
-import React from 'react';
+import brLocale from 'date-fns/locale/pt-BR'
 import DateFnsUtils from '@date-io/date-fns';
 import {
   MuiPickersUtilsProvider,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
+//Para editar as cores dos calendarios
+import { createMuiTheme } from '@material-ui/core'
+import { ThemeProvider } from "@material-ui/styles";
+import { blue } from "@material-ui/core/colors";
+
+//Variavel que define a COR do calendario
+const defaultMaterialTheme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+});
+
 export const DataOKCancel = (props) => {
-  
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+    <ThemeProvider theme={defaultMaterialTheme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={brLocale}>
         <KeyboardDatePicker
           margin="normal"
           label={props.label}
           format="dd/MM/yyyy"
           value={selectedDate}
           onChange={handleDateChange}
+          cancelLabel='Fechar'
+          invalidDateMessage="Data não é valida"
           KeyboardButtonProps={{
             'aria-label': 'change date',
           }}
         />
-    </MuiPickersUtilsProvider>
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>
   );
 }
 
 export const DataSemOKCancel = (props) => {
-  
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
   return (
-    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          label={props.label}
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-    </MuiPickersUtilsProvider>
+    <ThemeProvider theme={defaultMaterialTheme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={brLocale}>
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="dd/MM/yyyy"
+            margin="normal"
+            label={props.label}
+            value={selectedDate}
+            onChange={handleDateChange}
+            disablePast={true}
+            autoOk={true}
+            inputVariant='standard'
+            invalidDateMessage="Data não é valida"
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+      </MuiPickersUtilsProvider>
+    </ThemeProvider>  
   );
 }
