1b837d732c0856e88690822b6c617b058aceaf8b
maiquelveve <maiquelveve2008@gmail.com>
2020-05-25 00:14:53 -0300

    tela de registar

--- src/pages/Viagens/component/InformacoesViagens/index.js
+++ src/pages/Viagens/component/InformacoesViagens/index.js
@@ -3,37 +3,45 @@ import React from 'react';
 function InformacoesViagens(props) {
     return(
         <div className="container-fluid mt-2">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Reservar Viagem</h4>
-                    <p>Vagas: 4</p>
+            <div className="card">
+                <div className="card-header text-center">
+                    <div className="row">
+                        <p  className="col-lg-5 col-sm-12"><strong>ORIGEM:</strong> Canoas - RS</p>
+                        <p className="col-lg-2 col-sm-12">-></p>
+                        <p className="col-lg-5 col-sm-12"><strong>DESTINO:</strong> Quintão - RS</p>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <p><strong>Vagas Disponíveis:</strong> 4</p>
+                        </div>
+                    </div>    
                 </div>
-                <div class="card-body">
-                    <div class="row  text-center">
-                        <div class="col-sm-4">
+                <div className="card-body">
+                    <div className="row  text-center">
+                        <div className="col-sm-4">
                             <p>Origem: Canoas - RS</p>
                         </div>
-                        <div class="col-sm-4">
+                        <div className="col-sm-4">
                             <p>Destino: Quintão - RS</p>
                         </div>
-                        <div class="col-sm-2">
+                        <div className="col-sm-2">
                             <p>Data: 25/05/2020</p>
                         </div>
-                        <div class="col-sm-2">
+                        <div className="col-sm-2">
                             <p>Hora: 14:30</p>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-2  text-center">
+                    <div className="row">
+                        <div className="col-sm-2  text-center">
                             <p>Empresa: Fran Turs</p>
                         </div>
-                        <div class="col-sm-2  text-center">
+                        <div className="col-sm-2  text-center">
                             <p>Placa: iqy-3336</p>
                         </div>
-                        <div class="col-sm-4 text-center">
+                        <div className="col-sm-4 text-center">
                             <p>Valor: R$ 15,00</p>
                         </div>
-                        <div class="col-sm-4 text-center">
+                        <div className="col-sm-4 text-center">
                             <p>Situação: Viagem Confirmada </p>
                         </div>
                     </div>
